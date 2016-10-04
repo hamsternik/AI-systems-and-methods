b852ac8352b16b235af56dfb808d7ae233ac37df
Nikita Khomitsevich <hamsternik9@gmail.com>
2016-10-04 18:03:10 +0300

    [Lab1] Added AI Chat Bot project

---
+++ Lab1/AIChatBot/AIChatBot/MessageDB.swift
@@ -0,0 +1,52 @@
+//
+//  MessageDB.swift
+//  AIChatBot
+//
+//  Created by Nikita Khomitsevich on 10/1/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+import UIKit
+
+internal let messageAboutName = "Привет, меня зовут Лиза. Я интеллектуальная система, созданная для общения с людьми. А как твое имя?"
+internal let messageAboutAge = "Хорошо. Каков твой возраст? Введи лишь число!"
+
+// 0-5
+internal let messageChild1 = "Ух ничего себе, совсем маленький! Чем ты увлекаешься?"
+internal let messageChild2 = "Здорово! А как у тебя с поведением, хорошо себя ведешь?"
+internal let messageChild3 = "Тут главное - не расстраивать родителей :D. Ты, кстати, гуляешь на улице?"
+internal let messageChild4 = "Молодчинка. Извини, но мне пора. До встречи!"
+
+// 6-17
+internal let messageTeenager1 = "Как интересно! У тебя самый подходящий возраст что бы активно развиваться в чем-то. У тебя есть какие-то интересы?"
+internal let messageTeenager2 = "А как у тебя успехи в школе? Хорошо ли учишься?"
+internal let messageTeenager3 = "Ты занимаешься каким-либо видом спорта?"
+internal let messageTeenager4 = "Есть ли у тебя хобби?"
+internal let messageTeenager5 = "Молодчинка. Извини, но мне пора. До встречи!"
+
+// 18-23
+internal let messageYoung1 = "У тебя отличный возраст для начинаний! Ты сейчас учишься в ВУЗе?"
+internal let messageYoung2 = "Можешь уточнить его название?"
+internal let messageYoung3 = "Молодец. Ты сейчас на каком курсе?"
+internal let messageYoung4 = "Сложно ли тебе учиться?"
+internal let messageYoung5 = "Ясно. А чем ты занимаешься в свободное время?"
+internal let messageYoung6 = "Круто! Спасибо за общение, мне уже пора идти. До скорой встречи!"
+
+// 24-45
+internal let messageAdult1 = "Ты уже закончил университет?"
+internal let messageAdult2 = "Окей. А ты где-то работаешь?"
+internal let messageAdult3 = "Расскажи подробней. Это довольно интересно!"
+internal let messageAdult4 = "А чем ты занимаешься в свободное время?"
+internal let messageAdult5 = "Круто! Спасибо за общение, мне уже пора идти. До скорой встречи!"
+
+// 46-65
+internal let messageOld1 = "Чем ты занимаешься? Какая у тебя специальность?"
+internal let messageOld2 = "Довольно интересно! А есть ли у тебя семья?"
+internal let messageOld3 = "Прости, уточню - есть ли дети? Если да, то сколько?"
+internal let messageOld4 = "Есть ли у тебя хобби?"
+internal let messageOld5 = "Извини, но мне пора. До встречи!"
+
+// 66-100
+internal let messageOldest1 = "Ого, ничего себе! Ты, наверно, очень многое повидал в жизни! Чем ты занимаешься сейчас?"
+internal let messageOldest2 = "Я так понимаю, ты сейчас не работаешь?"
+internal let messageOldest3 = "Ой прости, что так неловко выходит, но мне нужно идти. Пока!"
---
+++ Lab1/AIChatBot/AIChatBot/ViewController.swift
@@ -0,0 +1,275 @@
+//
+//  ViewController.swift
+//  AIChatBot
+//
+//  Created by Nikita Khomitsevich on 9/30/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+enum Avatar {
+    case AIChatBot
+    case User
+}
+
+class ViewController: JSQMessagesViewController {
+
+    var messages = [JSQMessage]()
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    fileprivate var userName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+        outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.lightGray)
+        
+        collectionView?.collectionViewLayout.incomingAvatarViewSize = CGSize(width: kJSQMessagesCollectionViewAvatarSizeDefault, height:kJSQMessagesCollectionViewAvatarSizeDefault )
+        collectionView?.collectionViewLayout.outgoingAvatarViewSize = CGSize(width: kJSQMessagesCollectionViewAvatarSizeDefault, height:kJSQMessagesCollectionViewAvatarSizeDefault )
+        
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage.jsq_defaultTypingIndicator(), style: .plain, target: self, action: #selector(receiveMessagePressed))
+        
+        automaticallyScrollsToMostRecentMessage = true
+        
+        self.collectionView?.reloadData()
+        self.collectionView?.layoutIfNeeded()
+    }
+
+    // NARK: TEST - Remove it
+    /*
+    func receiveMessagePressed(_ sender: UIBarButtonItem) {
+        /**
+         *  DEMO ONLY
+         *
+         *  The following is simply to simulate received messages for the demo.
+         *  Do not actually do this.
+         */
+        
+        /**
+         *  Show the typing indicator to be shown
+         */
+        self.showTypingIndicator = !self.showTypingIndicator
+        
+        /**
+         *  Scroll to actually view the indicator
+         */
+        self.scrollToBottom(animated: true)
+        
+        /**
+         *  Copy last sent message, this will be the new "received" message
+         */
+        var copyMessage = self.messages.last?.copy()
+        
+        if (copyMessage == nil) {
+            copyMessage = JSQMessage(senderId: Avatar.AIChatBot, displayName: getName("AIChatBot"), text: "First received!")
+        }
+        
+        var newMessage:JSQMessage!
+        var newMediaData:JSQMessageMediaData!
+        var newMediaAttachmentCopy:AnyObject?
+        
+        if (copyMessage! as AnyObject).isMediaMessage() {
+            /**
+             *  Last message was a media message
+             */
+            let copyMediaData = (copyMessage! as AnyObject).media
+            
+            switch copyMediaData {
+            case is JSQPhotoMediaItem:
+                let photoItemCopy = (copyMediaData as! JSQPhotoMediaItem).copy() as! JSQPhotoMediaItem
+                photoItemCopy.appliesMediaViewMaskAsOutgoing = false
+                
+                newMediaAttachmentCopy = UIImage(cgImage: photoItemCopy.image!.cgImage!)
+                
+                /**
+                 *  Set image to nil to simulate "downloading" the image
+                 *  and show the placeholder view5017
+                 */
+                photoItemCopy.image = nil;
+                
+                newMediaData = photoItemCopy
+            case is JSQLocationMediaItem:
+                let locationItemCopy = (copyMediaData as! JSQLocationMediaItem).copy() as! JSQLocationMediaItem
+                locationItemCopy.appliesMediaViewMaskAsOutgoing = false
+                newMediaAttachmentCopy = locationItemCopy.location!.copy() as AnyObject?
+                
+                /**
+                 *  Set location to nil to simulate "downloading" the location data
+                 */
+                locationItemCopy.location = nil;
+                
+                newMediaData = locationItemCopy;
+            case is JSQVideoMediaItem:
+                let videoItemCopy = (copyMediaData as! JSQVideoMediaItem).copy() as! JSQVideoMediaItem
+                videoItemCopy.appliesMediaViewMaskAsOutgoing = false
+                newMediaAttachmentCopy = (videoItemCopy.fileURL! as NSURL).copy() as AnyObject?
+                
+                /**
+                 *  Reset video item to simulate "downloading" the video
+                 */
+                videoItemCopy.fileURL = nil;
+                videoItemCopy.isReadyToPlay = false;
+                
+                newMediaData = videoItemCopy;
+            case is JSQAudioMediaItem:
+                let audioItemCopy = (copyMediaData as! JSQAudioMediaItem).copy() as! JSQAudioMediaItem
+                audioItemCopy.appliesMediaViewMaskAsOutgoing = false
+                newMediaAttachmentCopy = (audioItemCopy.audioData! as NSData).copy() as AnyObject?
+                
+                /**
+                 *  Reset audio item to simulate "downloading" the audio
+                 */
+                audioItemCopy.audioData = nil;
+                
+                newMediaData = audioItemCopy;
+            default:
+                assertionFailure("Error: This Media type was not recognised")
+            }
+            
+            newMessage = JSQMessage(senderId: AvatarIdJobs, displayName: getName(User.Jobs), media: newMediaData)
+        }
+        else {
+            /**
+             *  Last message was a text message
+             */
+            
+            newMessage = JSQMessage(senderId: AvatarIdJobs, displayName: getName(User.Jobs), text: (copyMessage! as AnyObject).text)
+        }
+        
+        /**
+         *  Upon receiving a message, you should:
+         *
+         *  1. Play sound (optional)
+         *  2. Add new JSQMessageData object to your data source
+         *  3. Call `finishReceivingMessage`
+         */
+        
+        self.messages.append(newMessage)
+        self.finishReceivingMessage(animated: true)
+    }
+ */
+    
+    
+    // MARK: JSQMessagesViewController
+    
+    override func didPressSend(_ button: UIButton, withMessageText text: String, senderId: String, senderDisplayName: String, date: Date) {
+        let message = JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)
+        self.messages.append(message)
+        self.finishSendingMessage(animated: true)
+    }
+    
+    //MARK: JSQMessages CollectionView DataSource
+    
+    override func senderId() -> String {
+        return "309-41802-93823"
+    }
+    
+    override func senderDisplayName() -> String {
+        return "Steve Wazniak"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageDataForItemAt indexPath: IndexPath) -> JSQMessageData {
+        return messages[(indexPath as NSIndexPath).item]
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageBubbleImageDataForItemAt indexPath: IndexPath) -> JSQMessageBubbleImageDataSource {
+        
+        return messages[(indexPath as NSIndexPath).item].senderId == self.senderId() ? outgoingBubble : incomingBubble
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, avatarImageDataForItemAt indexPath: IndexPath) -> JSQMessageAvatarImageDataSource? {
+        let message = messages[(indexPath as NSIndexPath).item]
+        return JSQMessagesAvatarImageFactory().avatarImage(withUserInitials: "SW", backgroundColor: UIColor.jsq_messageBubbleGreen(), textColor: UIColor.white, font: UIFont.systemFont(ofSize: 12))
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, attributedTextForCellTopLabelAt indexPath: IndexPath) -> NSAttributedString? {
+        /**
+         *  This logic should be consistent with what you return from `heightForCellTopLabelAtIndexPath:`
+         *  The other label text delegate methods should follow a similar pattern.
+         *
+         *  Show a timestamp for every 3rd message
+         */
+        if ((indexPath as NSIndexPath).item % 3 == 0) {
+            let message = self.messages[(indexPath as NSIndexPath).item]
+            
+            return JSQMessagesTimestampFormatter.shared().attributedTimestamp(for: message.date)
+        }
+        
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath) -> NSAttributedString? {
+        let message = messages[(indexPath as NSIndexPath).item]
+        
+        // Displaying names above messages
+        //Mark: Removing Sender Display Name
+        /**
+         *  Example on showing or removing senderDisplayName based on user settings.
+         *  This logic should be consistent with what you return from `heightForCellTopLabelAtIndexPath:`
+         */
+//        if defaults.bool(forKey: Setting.removeSenderDisplayName.rawValue) {
+//            return nil
+//        }
+        
+        if message.senderId == self.senderId() {
+            return nil
+        }
+        
+        return NSAttributedString(string: message.senderDisplayName)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout, heightForCellTopLabelAt indexPath: IndexPath) -> CGFloat {
+        /**
+         *  Each label in a cell has a `height` delegate method that corresponds to its text dataSource method
+         */
+        
+        /**
+         *  This logic should be consistent with what you return from `attributedTextForCellTopLabelAtIndexPath:`
+         *  The other label height delegate methods should follow similarly
+         *
+         *  Show a timestamp for every 3rd message
+         */
+        if (indexPath as NSIndexPath).item % 3 == 0 {
+            return kJSQMessagesCollectionViewCellLabelHeightDefault
+        }
+        
+        return 0.0
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout, heightForMessageBubbleTopLabelAt indexPath: IndexPath) -> CGFloat {
+        
+        /**
+         *  Example on showing or removing senderDisplayName based on user settings.
+         *  This logic should be consistent with what you return from `attributedTextForCellTopLabelAtIndexPath:`
+         */
+//        if defaults.bool(forKey: Setting.removeSenderDisplayName.rawValue) {
+//            return 0.0
+//        }
+        
+        /**
+         *  iOS7-style sender name labels
+         */
+        let currentMessage = self.messages[(indexPath as NSIndexPath).item]
+        
+        if currentMessage.senderId == self.senderId() {
+            return 0.0
+        }
+        
+        if (indexPath as NSIndexPath).item - 1 > 0 {
+            let previousMessage = self.messages[(indexPath as NSIndexPath).item - 1]
+            if previousMessage.senderId == currentMessage.senderId {
+                return 0.0
+            }
+        }
+        
+        return kJSQMessagesCollectionViewCellLabelHeightDefault;
+    }
+}
+
